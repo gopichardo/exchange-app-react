718552418e7a3be2f2c43d304d1bfc472fe98374
Cesar GONZALEZ <cesar.gonzalez@consulting-for.edenred.com>
2022-01-03 16:55:50 -0600

    Se inicializa repositorio
    Se modifica estructura del proyecto
    Se agrega componente header
    Se agrega fuente Montserrat
    Se agrega imagen de fondo

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "../assets/css/Header.css";
+
+class Header extends Component {
+  render() {
+    return <a class="brand" href="/">Exchange</a>;
+  }
+}
+
+export default Header;
