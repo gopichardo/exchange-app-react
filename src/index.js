49d9dc24859dfef8cec83712a780819c1447b50d
Cesar GONZALEZ <cesar.gonzalez@consulting-for.edenred.com>
2022-01-04 19:14:14 -0600

    Se agrega bulma container, columns Se remueve padding de body

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './assets/css/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "./assets/css/grid.css"
 
 ReactDOM.render(
   <React.StrictMode>
