49e758a5e7ad5ef4a85ddd173c524fccc6946a47
Cesar <gopichardoces@gmail.com>
2022-01-05 18:12:52 -0600

    Se agrega bulma typography, spacing Se modifica estructura de Exchangerate

--- src/components/ExchangeRate.js
+++ src/components/ExchangeRate.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import Select from 'react-select'
+import Select from 'react-select';
+import "../assets/css/ExchangeRate.css";
 
 class EchangeRate extends Component {
 
@@ -14,20 +15,21 @@ class EchangeRate extends Component {
 
 
     render() {
-        return <div>
-            <h1 className="title">
+        return <div className="column">
+            <h1 className="title is-size-4-mobile">
                 Summary of <b>Mexican Peso</b> exchange rates against other currencies.
             </h1>
-            <label htmlFor="sourceCurrency">Compare</label>
-            <Select options={this.options} />
-            {/* <select id="sourceCurrency" name="sourceCurrency">
-                <option value="MXN">Peso Mexicano - MXN</option>
-            </select> */}
-            <label htmlFor="destinationCurrency">to</label>
-            <Select options={this.options} />
-            {/* <select id="sourceCurrency" name="sourceCurrency">
-                <option value="USD">United States Dollar - USD</option>
-            </select> */}
+            <div className="has-text-centered pb-3">
+                <label htmlFor="sourceCurrency" className="is-size-5-mobile">Compare</label>
+            </div>
+            <Select options={this.options} className="pb-3" />
+            <div className="has-text-centered pb-3">
+                <label htmlFor="destinationCurrency" className="is-size-5-mobile">to</label>
+            </div>
+            <Select options={this.options} className="pb-3" />
+            <div className="column big-card">
+                X
+            </div>
         </div>;
     }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import "../assets/css/Header.css";
 class Header extends Component {
   render() {
     return (
-      <a className="brand" href="/">
+      <a className="brand column is-size-1-mobile" href="/">
         Exchange
       </a>
     );
