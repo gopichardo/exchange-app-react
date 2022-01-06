226d0d4f5bdaed4dfe72b7ef4cd60f53484a2f8c
Cesar GONZALEZ <cesar.gonzalez@consulting-for.edenred.com>
2022-01-06 17:12:40 -0600

    Se agregan props a currency Se agrega lista de monedas a ExchangeRate Se corrige Footer

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -1,9 +1,35 @@
 import React, { Component } from "react";
 import "../assets/css/Currency.css";
+import c3 from "c3";
 
 class Currency extends Component {
   render() {
-    return <div className="small-card column block">USD</div>;
+    return (
+      <div className="small-card column block">
+        <p className="is-size-5-mobile has-text-weight-bold">{this.props.symbol}</p>
+        <p className="is-size-5-mobile">{this.props.currency}</p>
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    this.generateChart();
+  }
+
+  generateChart() {
+    c3.generate({
+      bindto: "#mainChart",
+      data: {
+        columns: [["data1", 20, 50, 80, 98]],
+        type: "area-spline",
+        colors: {
+          data1: "#2B5876",
+        },
+      },
+      point: {
+        r: 3,
+      },
+    });
   }
 }
 
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,8 +5,8 @@ import c3 from "c3";
 class Footer extends Component {
   render() {
     return (
-      <footer class="footer">
-        <div class="content has-text-centered is-size-7-mobile">
+      <footer className="footer">
+        <div className="content has-text-centered is-size-7-mobile">
           Powered by <strong>gopichardoces</strong>
         </div>
       </footer>
